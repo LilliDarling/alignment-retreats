01f1d9ede13c979ac6deab7312252888f68f08b3
Lilli <valkyrieremedy@gmail.com>
2026-02-27 18:24:34 -0700

    security

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Activate preloaded font stylesheets (loaded as media="print" to avoid render-blocking)
+document.querySelectorAll<HTMLLinkElement>('link[rel="stylesheet"][media="print"]').forEach(link => {
+  link.media = 'all';
+});
+
 createRoot(document.getElementById("root")!).render(<App />);
